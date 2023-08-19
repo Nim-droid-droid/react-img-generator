521c1cdf4259f5c8c970889b82adbbad267d79a6
Nim-droid-droid <levkoz2222@outlook.com>
2023-08-19 20:02:22 +0100

    Made modifications

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from "react"
 export default function Header(){
   
   return(
-    <header>
+    <header className="header">
+    {/* sailor moon logo */}
+      <img src="" alt="" />
+      <h2>Sailor Moon Image Generator</h2>
     </header>
   )
 }
