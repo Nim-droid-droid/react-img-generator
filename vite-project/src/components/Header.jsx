140df3dd779bf0cc1ac4c430d965ef2550772d4a
Nim-droid-droid <levkoz2222@outlook.com>
2023-08-20 18:24:15 +0100

    Made modifications

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react"
+// import React from "react"  No longer need to do this for funt components, just for Class components cuz I need the life-cycle methods from React.
 
 export default function Header(){
   
   return(
+
     <header className="header">
+
     </header>
   )
 }
