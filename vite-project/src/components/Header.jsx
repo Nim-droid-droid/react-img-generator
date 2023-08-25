35aec0904b979eb147fecfff8560d6e3b93562f3
Nim-droid-droid <levkoz2222@outlook.com>
2023-08-25 19:40:35 +0100

    Made modifications

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -5,6 +5,7 @@ export default function Header(){
   return(
     // Didn't use fragments cuz I'll be applying style to this
     <header className="header">
+      {/* sailor moon logo */}
 
     </header>
   )
