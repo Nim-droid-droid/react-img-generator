ffa3d6aa92f24ea1a18eaa8b1ba9330f326c4695
Nim-droid-droid <levkoz2222@outlook.com>
2023-09-18 22:43:32 +0100

    Imported Theme context via useTheme funt

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Form from './components/Form'
-import { ThemeProvider } from './context/ThemeContext'
+import { ThemeProvider, useTheme } from './context/ThemeContext'
 
 
 export default function App() {
